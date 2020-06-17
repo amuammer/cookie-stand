b3d8db6e11585136da227f54786da0a7e8b48003
amuammer <amuammer@students.iugaza.edu.ps>
2020-06-17 13:43:09 +0300

    adding form

--- js/app.js
+++ js/app.js
@@ -2,17 +2,19 @@ function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min) + 1);
 }
 
-function Branch(location, minCustomers, maxCustomers, avgCookies) {
-    this.location = location;
+function Location(name, minCustomers, maxCustomers, avgCookies) {
+    this.name = name;
     this.minCustomers = minCustomers;
     this.maxCustomers = maxCustomers;
     this.avgCookies = avgCookies;
     this.data = [];
     this.total = 0;
+    this.generateData();
+    this.render();
 }
 
 const hourlyTotal = [];
-Branch.prototype.generateData = function(){
+Location.prototype.generateData = function(){
   for (let i = 0; i <= 13; i++) {
     const randomCustomers = randomBetween(this.minCustomers, this.maxCustomers);
     const randomCookies = Math.round(randomCustomers * this.avgCookies);
@@ -24,11 +26,11 @@ Branch.prototype.generateData = function(){
   }
 }
 
-Branch.prototype.render = function() {
+Location.prototype.render = function() {
   const table =  document.getElementById("table");
   const tr = document.createElement("tr");
   const location = document.createElement("td");
-  location.innerText = this.location;
+  location.innerText = this.name;
   tr.appendChild(location);
   for (var i = 0; i < this.data.length; i++) {
     const td = document.createElement("td");
@@ -41,44 +43,27 @@ Branch.prototype.render = function() {
   table.appendChild(tr);
 }
 
-const Seattle = new Branch("Seattle", 23, 65, 6.3);
+const Seattle = new Location("Seattle", 23, 65, 6.3);
+const Tokyo = new Location("Tokyo", 3, 24, 1.2);
+const Dubai = new Location("Dubai", 11, 38, 3.7);
+const Paris = new Location("Paris", 20, 38, 2.3);
+const Lima = new Location("Lima", 2, 16, 4.6);
 
-const Tokyo = new Branch("Tokyo", 3, 24, 1.2);
-
-const Dubai = new Branch("Dubai", 11, 38, 3.7);
-
-const Paris = new Branch("Paris", 20, 38, 2.3);
-
-const Lima = new Branch("Lima", 2, 16, 4.6);
-
-
-// Seattle
-Seattle.generateData();
-Seattle.render();
-
-// Tokyo
-Tokyo.generateData();
-Tokyo.render();
-
-// Dubai
-Dubai.generateData();
-Dubai.render();
-
-// Paris
-Paris.generateData();
-Paris.render();
-
-// Lima
-Lima.generateData();
-Lima.render();
 
 function renderHourlyTotal(){
+  try {
+    const element = document.getElementById("totalsRow");
+    element.parentNode.removeChild(element);
+  } catch (e) {
+    console.log(e);
+  }
   const table =  document.getElementById("table");
   const tr = document.createElement("tr");
+  tr.id = "totalsRow";
   const location = document.createElement("td");
   location.innerText = "Totals";
   tr.appendChild(location);
-  for (var i = 0; i < hourlyTotal.length; i++) {
+  for (let i = 0; i < hourlyTotal.length; i++) {
     const td = document.createElement("td");
     td.innerText = hourlyTotal[i];
     tr.appendChild(td);
@@ -89,5 +74,18 @@ function renderHourlyTotal(){
   tr.appendChild(total);
   table.appendChild(tr);
 }
-
 renderHourlyTotal();
+
+function addLocation(event){
+  event.preventDefault();
+  const locationName = event.target.name.value;
+  const minCustomers = parseInt(event.target.minCustomers.value, 10);
+  const maxCustomers = parseInt(event.target.maxCustomers.value, 10);
+  const avgCookies = parseFloat(event.target.avgCookies.value, 10);
+  new Location(locationName, minCustomers, maxCustomers, avgCookies);
+  renderHourlyTotal();
+  alert("success");
+}
+
+const addLocationForm = document.getElementById("addLocationForm");
+document.addEventListener("submit", addLocation);
